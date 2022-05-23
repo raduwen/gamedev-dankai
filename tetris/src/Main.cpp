b8270f83aec6be9a412e1a914118bc47ed66b2ca
raduwen <raduwen@gmail.com>
2022-05-23 20:57:12 +0900

    level2

--- tetris/src/Main.cpp
+++ tetris/src/Main.cpp
@@ -3,16 +3,35 @@
 int main() {
   sf::RenderWindow window(sf::VideoMode(800, 600), "Tetris");
 
+  sf::RectangleShape block{{32, 32}};
+  block.setFillColor(sf::Color::Red);
+  block.setOutlineColor({64, 0, 0});
+  block.setOutlineThickness(1.f);
+
   while (window.isOpen()) {
+    // process events
     sf::Event event;
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed)
         window.close();
     }
+
+    // update
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
       window.close();
 
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+      block.move(-0.1, 0);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+      block.move(0.1, 0);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+      block.move(0, -0.1);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+      block.move(0, 0.1);
+
+    // render
     window.clear();
+    window.draw(block);
     window.display();
   }
 
