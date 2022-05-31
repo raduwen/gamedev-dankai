ea6a2422203b8bbfe97319514c3b8121fc106800
raduwen <raduwen@gmail.com>
2022-05-31 21:05:28 +0900

    move ball

--- arkanoid/src/Ball.cpp
+++ arkanoid/src/Ball.cpp
@@ -3,9 +3,20 @@
 Ball::Ball() : sf::CircleShape(16.f) { setFillColor(sf::Color::Red); }
 
 void Ball::update(const sf::Time &deltaTime) {
-  // move
+  // get velocity
   auto v = direction_ * (speed_ * deltaTime.asSeconds());
-  move(v);
 
-  // [tood] - collision
+  // reflect when hit wall
+  if (getPosition().x + v.x < 0) {
+    direction_.x *= -1;
+  } else if (getPosition().x + v.x > 1600 - getRadius() * 2) {
+    direction_.x *= -1;
+  }
+  if (getPosition().y + v.y < 0) {
+    direction_.y *= -1;
+  } else if (getPosition().y + v.y > 900 - getRadius() * 2) {
+    direction_.y *= -1;
+  }
+
+  move(v);
 }
